756b0a6ed707ebd72286a198baa53672972b1e66
Anton Evdokimov <free0u@gmail.com>
2013-04-02 00:28:06 +0400

    Print all args with stdio library

--- dupme/main.c
+++ dupme/main.c
@@ -1,3 +1,10 @@
-int main() {
-	return 0;
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+    printf("%d args\n", argc);
+    int i;
+    for (i = 0; i < argc; ++i) {
+        printf("%s\n", argv[i]);
+    }
+    return 0;
 }
