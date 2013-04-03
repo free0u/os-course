96785403d397787999f4be03c3a5297af800a88a
free0u <free0u@gmail.com>
2013-04-03 23:04:32 +0400

    Add double write section and some "if" structure

--- dupme/main.c
+++ dupme/main.c
@@ -1,3 +1,4 @@
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -21,18 +22,47 @@ int parse_int(char* s) {
 }
 
 int main(int argc, char* argv[]) {
-    int k = parse_int(argv[1]);
+    int k = parse_int(argv[1]) + 1;
 
     char* buffer = malloc(k);
     int result;
 
+    /*
     while (1) {
-        result = read(0, buffer, 1);
-        printf("%d\n", result);
-
-        if (result == 0) {
-            break;
+        int cc = read(0, buffer, k);
+        printf("%d\n", cc);
+    }
+    return 0;
+    */
+    int len = 0;
+    while (1) {
+        // invariant: buffer contains "len" chars of new (and maybe next) strings
+        if (len == k) { // buffer is full
+            int ind_endl = -1;
+            int i;
+            for (i = 0; i < len; ++i) {
+                if (buffer[i] == '\n') {
+                    ind_endl = i;
+                    break;
+                }
+            }
+            if (ind_endl != -1) { // buffer contain endl
+                write(1, buffer, ind_endl); printf("\n");
+                write(1, buffer, ind_endl); printf("\n");
+                if (ind_endl < len - 1) { // copy tail to begin (without endl)
+                    memmove(buffer, buffer + ind_endl + 1, len - ind_endl - 1);
+                    len = len - ind_endl - 1;
+                } else 
+                {
+                    len = 0;
+                }
+            } else 
+            {
+            }
         }
+        int count = read(0, buffer + len, k - len);
+        printf("%d\n", count);
+        len += count;
     }
 
     free(buffer);
