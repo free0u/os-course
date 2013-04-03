60ccc17be03d01495d60413fbd127fb57e868dab
free0u <free0u@gmail.com>
2013-04-03 16:13:08 +0400

    Add _strlen function

--- dupme/main.c
+++ dupme/main.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
 
+int _strlen(char* s) {
+    int i = 0;
+    while (s[i] != 0) {
+        ++i;
+    }
+    return i;
+}
+
 int main(int argc, char* argv[]) {
     printf("%d args\n", argc);
     int i;
     for (i = 0; i < argc; ++i) {
-        printf("%s\n", argv[i]);
+       // printf("%s\n", argv[i]);
+        printf("%d\n", _strlen(argv[i]));
     }
     return 0;
 }
