c078434e6ddf32f98ad57e71b210e9f11d7416a4
free0u <free0u@gmail.com>
2013-04-03 18:12:08 +0400

    Add parse_int function. Exit from program when expected EOF

--- dupme/main.c
+++ dupme/main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 int _strlen(char* s) {
     int i = 0;
@@ -8,12 +10,31 @@ int _strlen(char* s) {
     return i;
 }
 
-int main(int argc, char* argv[]) {
-    printf("%d args\n", argc);
-    int i;
-    for (i = 0; i < argc; ++i) {
-       // printf("%s\n", argv[i]);
-        printf("%d\n", _strlen(argv[i]));
+int parse_int(char* s) {
+    int result = 0;
+    int i = 0;
+    while (s[i] != 0) {
+        result = result * 10 + (s[i] - '0');
+        ++i;
     }
+    return result;
+}
+
+int main(int argc, char* argv[]) {
+    int k = parse_int(argv[1]);
+
+    char* buffer = malloc(k);
+    int result;
+
+    while (1) {
+        result = read(0, buffer, 1);
+        printf("%d\n", result);
+
+        if (result == 0) {
+            break;
+        }
+    }
+
+    free(buffer);
     return 0;
 }
