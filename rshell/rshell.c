8ed05540f149cf7b78d88b38870911d5e5f641b3
Anton Evdokimov <free0u@gmail.com>
2013-05-24 18:32:03 +0400

    Supported many of clients

--- rshell/rshell.c
+++ rshell/rshell.c
@@ -6,13 +6,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <signal.h>
+
+int child_pid = 0;
+
+void handler(int foo) {
+    if (child_pid)
+        kill(-child_pid, SIGINT);
+}
 
 int main(void)
 {
+//    child_pid = fork();
+//    if (child_pid == 0) {
+//        setsid();
+//    } else
+//    {
+//        signal(SIGINT, &handler);
+//    } 
+
     struct sockaddr_storage their_addr;
     socklen_t addr_size;
     struct addrinfo hints, *res;
-    int sockfd, new_fd;
+    int sockfd;
 
     memset(&hints, 0, sizeof hints);
     hints.ai_family = AF_UNSPEC;  // use IPv4 or IPv6, whichever
@@ -20,8 +36,6 @@ int main(void)
     hints.ai_flags = AI_PASSIVE;     // fill in my IP for me
 
     getaddrinfo(NULL, "8823", &hints, &res);
-
-    // создадим сокет, биндим его, и слушаем:
 
     sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
     int status = 1;
@@ -33,19 +47,28 @@ int main(void)
     listen(sockfd, 10);
 
     addr_size = sizeof their_addr;
-    new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &addr_size);
 
-    int pid = fork();
+    while (true) {
+        printf("Waiting for accept\n");
+        int new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &addr_size);
 
-    if (pid == 0) {
-        dup2(new_fd, 0);
-        dup2(new_fd, 1);
-        dup2(new_fd, 2);
+        int pid = fork();
+        if (pid == 0) {
+            close(sockfd);
 
-        printf("Hello, world\n");
-    } else
-    {
-        close(sockfd);
-        close(new_fd);
+            dup2(new_fd, 0);
+            dup2(new_fd, 1);
+            dup2(new_fd, 2);
+    
+            printf("Hello, world\n");
+
+            close(new_fd);
+            return 0;
+        } else
+        {
+            close(new_fd);
+        }
     }
+    close(sockfd);
+    return 0;
 }
