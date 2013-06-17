e4276b208710eacc37aec59a10356df91b6bfa51
Anton Evdokimov <free0u@gmail.com>
2013-06-17 16:12:27 +0400

    Added working code

--- offtp/offtp.c
+++ offtp/offtp.c
@@ -1,8 +1,10 @@
 #include <string.h>
-#include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <netinet/in.h>
 #include <netdb.h>
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -15,21 +17,46 @@
 
 int child_pid = 0;
 
+int check(const char * message, int what) {
+    if (what < 0) {
+        perror(message);
+        _exit(1);
+    }
+    return what;
+}
+
 void handler(int foo) {
     (void)foo;
     if (child_pid)
         kill(-child_pid, SIGINT);
 }
 
-int write_all(int file, char * buf, int cnt) {
+int find_char(char* s, int len, char c) {
+    int i = 0;
+    while (i < len) {
+        if (s[i] == c) return i;
+        ++i;
+    }
+    return -1;
+}
+
+int write_all(int file, const char * buf, int cnt) {
     int st = 0;
-    while(cnt > 0) {
-        int sz = write(file, buf + st, cnt);
-        if (sz == -1) return -1;
+    while(st < cnt) {
+        int sz = check("write", write(file, buf + st, cnt));
         st += sz;
-        cnt -= sz;
     } 
     return 0;
+}
+
+int get_file_size(const char * name) {
+    struct stat st;
+    stat(name, &st);
+    return st.st_size;
+}
+
+void itoa(int value, char * buf) {
+    sprintf(buf, "%d", value);
 }
 
 int main(void)
@@ -76,11 +103,46 @@ int main(void)
         // Waiting for accept
         int new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &addr_size);
 
-        char buffer[4096];
-        while (true) {
-            int cnt = read(new_fd, buffer, 4096);
-            if (cnt < 1) break;
-            write(new_fd, buffer, cnt);
+        int pid = fork();
+        if (pid == 0) { //child
+            close(sockfd);
+            const int BUFFER_SIZE = 4096;
+            char buffer[BUFFER_SIZE];
+
+            int pos = 0;
+            while (true) {
+                int cnt = check("read err", read(new_fd, buffer + pos, BUFFER_SIZE - pos));
+                if (cnt == 0) break;
+                pos += cnt;
+                if (find_char(buffer, pos, 0) != -1) break;
+            } 
+
+            char buf_size[20];
+            itoa(get_file_size(buffer), buf_size);
+
+            int f = open(buffer, O_RDONLY, S_IREAD | S_IWRITE);
+            if (f == -1) {
+                char * err = strerror(errno);
+                write_all(new_fd, "err: ", 5);
+                write_all(new_fd, err, strlen(err));
+                close(new_fd);
+                return 0;
+            }
+            write_all(new_fd, "ok\0", 3);
+            write_all(new_fd, buf_size, strlen(buf_size));
+            
+            while (true) {
+                int cnt = check("read err", read(f, buffer, BUFFER_SIZE));
+                if (cnt == 0) break;
+                write_all(new_fd, buffer, cnt);
+            }
+            close(f);
+
+            close(new_fd);
+            return 0;
+        } else
+        {
+            close(new_fd);
         }
     }
     close(sockfd);
